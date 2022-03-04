46d370e298944d0a106bcf855aba0c111d5dcb65
Ubuntu <ubuntu@ip-172-31-85-213.ec2.internal>
2022-03-04 03:50:45 +0000

    Area and perimeter of a circle

---
+++ areaPerCirc.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) Year Your Name All rights reserved.
+
+//
+
+// Created by: Ferdaws
+
+// Date: 3/3/2022
+
+// This program shows the area and perimeter of a circle with radius of 15mm.
+
+
+
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  std::cout << "A circle with a radius \n";
+  std::cout << "of 15mm:\n";
+  std::cout << "\n";
+  std::cout << "Area = " << M_PI * pow(15 , 2)  << " cm^2\n";
+  std::cout << "Perimeter = " << 2*M_PI*15 << " cm\n";
+}
